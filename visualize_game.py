bf9811f7b149e872259e082b85533997c18e9074
Lílian <lily.mosquitoes@gmail.com>
2021-07-18 01:07:44 -0300

    changed game visualization to read json

--- visualize_game.py
+++ visualize_game.py
@@ -1,43 +1,50 @@
 import pygame
 import sys
+import json
+import time
 
-window_size = (300, 300)
+class Playback():
+    def __init__(self, board_size, steps, performance):
+        print('steps: ', steps)
+        print('performance: ', performance)
+        board_size = (board_size[0]*10+10, board_size[1]*10+10)
 
-black = pygame.Color(0, 0, 0)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
+        self.black = pygame.Color(0, 0, 0)
+        self.red = pygame.Color(255, 0, 0)
+        self.green = pygame.Color(0, 255, 0)
 
+        pygame.init()
+        pygame.display.set_caption('snake game visualizer')
 
-pygame.init()
-pygame.display.set_caption('snake game visualizer')
-window = pygame.display.set_mode(window_size)
+        self.window = pygame.display.set_mode(board_size)
+        self.fps = pygame.time.Clock()
 
-fps = pygame.time.Clock()
+    def draw_board(self, board_position):
+        self.window.fill(self.black)
 
-def move(states_list):
-    window.fill(black)
+        food_x = board_position['food_pos'][0]*10
+        food_y = board_position['food_pos'][1]*10
+        pygame.draw.rect(self.window, self.red, pygame.Rect(food_x-1, food_y-1, 10-1, 10-1))
 
-    snake = next(states_list)
-    for x, y in snake:
-        pygame.draw.rect(window, green, pygame.Rect(x-1, y-1, 10-1, 10-1))
+        snake = [[tile[0]*10, tile[1]*10] for tile in board_position['snake']]
+        for x, y in snake:
+            pygame.draw.rect(self.window, self.green, pygame.Rect(x-1, y-1, 10-1, 10-1))
 
-    pygame.display.update()
-    fps.tick(3)
-
-def open_game(file):
-    game_moves = open(file, 'rt').read().strip().split('\n')
-    states_list = list()
-    for move in game_moves:
-        move = move.split(';')
-        tiles = list()
-        for snake in move:
-            tiles.append([int(p) for p in snake.split(',')])
-        states_list.append(tiles)
-
-    return iter(states_list)
+        pygame.display.update()
+        self.fps.tick(3)
 
 if __name__ == '__main__':
-    states_list = open_game(sys.argv[1])
+    dump = open(sys.argv[1], 'r').read().split('\n')
+    game = [json.loads(line) for line in dump]
+    metadata = game.pop()
 
+    playback = Playback(**metadata)
+
+    board_position = iter(game)
     while True:
-        move(states_list)
+        try:
+            playback.draw_board(next(board_position))
+        except StopIteration:
+            break
+
+    time.sleep(1)
